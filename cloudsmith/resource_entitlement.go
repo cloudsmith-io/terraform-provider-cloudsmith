60dd5e8522136a094fb74de515694a64f7cb8852
Patrick Carey <pcarey@cloudsmith.io>
2020-06-24 15:58:01 +0100

    Add basic entitlement resource

---
+++ cloudsmith/resource_entitlement.go
@@ -0,0 +1,242 @@
+package cloudsmith
+
+import (
+	"github.com/antihax/optional"
+	"github.com/cloudsmith-io/cloudsmith-api-go"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func getIsActiveFromResource(d *schema.ResourceData) *bool {
+	var isActivePtr *bool
+
+	isActive, exists := d.GetOkExists("is_active") //nolint:staticcheck
+	if exists {
+		isActiveBool := isActive.(bool)
+		isActivePtr = &isActiveBool
+	}
+
+	return isActivePtr
+}
+
+func resourceEntitlementCreate(d *schema.ResourceData, m interface{}) error {
+	pc := m.(*providerConfig)
+
+	isActive := getIsActiveFromResource(d)
+	limitDateRangeFrom := d.Get("limit_date_range_from").(string)
+	limitDateRangeTo := d.Get("limit_date_range_to").(string)
+	limitNumClients := int64(d.Get("limit_num_clients").(int))
+	limitNumDownloads := int64(d.Get("limit_num_downloads").(int))
+	limitPackageQuery := d.Get("limit_package_query").(string)
+	limitPathQuery := d.Get("limit_path_query").(string)
+	namespace := d.Get("namespace").(string)
+	repository := d.Get("repository").(string)
+	name := d.Get("name").(string)
+	token := d.Get("token").(string)
+
+	opts := &cloudsmith.EntitlementsCreateOpts{
+		Data: optional.NewInterface(cloudsmith.EntitlementsCreate{
+			IsActive:           isActive,
+			LimitDateRangeFrom: limitDateRangeFrom,
+			LimitDateRangeTo:   limitDateRangeTo,
+			LimitNumClients:    limitNumClients,
+			LimitNumDownloads:  limitNumDownloads,
+			LimitPackageQuery:  limitPackageQuery,
+			LimitPathQuery:     limitPathQuery,
+			Name:               name,
+			Token:              token,
+		}),
+		ShowTokens: optional.NewBool(true),
+	}
+
+	entitlement, _, err := pc.APIClient.EntitlementsApi.EntitlementsCreate(pc.Auth, namespace, repository, opts)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(entitlement.SlugPerm)
+
+	return resourceEntitlementRead(d, m)
+}
+
+func resourceEntitlementRead(d *schema.ResourceData, m interface{}) error {
+	pc := m.(*providerConfig)
+
+	namespace := d.Get("namespace").(string)
+	repository := d.Get("repository").(string)
+
+	opts := &cloudsmith.EntitlementsReadOpts{
+		ShowTokens: optional.NewBool(true),
+	}
+
+	entitlement, _, err := pc.APIClient.EntitlementsApi.EntitlementsRead(pc.Auth, namespace, repository, d.Id(), opts)
+	if err != nil {
+		if err.Error() == errMessage404 {
+			d.SetId("")
+			return nil
+		}
+
+		return err
+	}
+
+	d.Set("is_active", entitlement.IsActive)
+	d.Set("limit_date_range_from", entitlement.LimitDateRangeFrom)
+	d.Set("limit_date_range_to", entitlement.LimitDateRangeTo)
+	d.Set("limit_num_clients", entitlement.LimitNumClients)
+	d.Set("limit_num_downloads", entitlement.LimitNumDownloads)
+	d.Set("limit_package_query", entitlement.LimitPackageQuery)
+	d.Set("limit_path_query", entitlement.LimitPathQuery)
+	d.Set("name", entitlement.Name)
+	d.Set("token", entitlement.Token)
+
+	// namespace and repository are not returned from the entitlement read
+	// endpoint, so we can use the values stored in resource state. We rely on
+	// ForceNew to ensure if either changes a new resource is created.
+	d.Set("namespace", namespace)
+	d.Set("repository", repository)
+
+	return nil
+}
+
+func resourceEntitlementUpdate(d *schema.ResourceData, m interface{}) error {
+	pc := m.(*providerConfig)
+
+	isActive := getIsActiveFromResource(d)
+	limitDateRangeFrom := d.Get("limit_date_range_from").(string)
+	limitDateRangeTo := d.Get("limit_date_range_to").(string)
+	limitNumClients := int64(d.Get("limit_num_clients").(int))
+	limitNumDownloads := int64(d.Get("limit_num_downloads").(int))
+	limitPackageQuery := d.Get("limit_package_query").(string)
+	limitPathQuery := d.Get("limit_path_query").(string)
+	namespace := d.Get("namespace").(string)
+	repository := d.Get("repository").(string)
+	name := d.Get("name").(string)
+	token := d.Get("token").(string)
+
+	opts := &cloudsmith.EntitlementsPartialUpdateOpts{
+		Data: optional.NewInterface(cloudsmith.EntitlementsPartialUpdate{
+			IsActive:           isActive,
+			LimitDateRangeFrom: limitDateRangeFrom,
+			LimitDateRangeTo:   limitDateRangeTo,
+			LimitNumClients:    limitNumClients,
+			LimitNumDownloads:  limitNumDownloads,
+			LimitPackageQuery:  limitPackageQuery,
+			LimitPathQuery:     limitPathQuery,
+			Name:               name,
+			Token:              token,
+		}),
+		ShowTokens: optional.NewBool(true),
+	}
+
+	entitlement, _, err := pc.APIClient.EntitlementsApi.EntitlementsPartialUpdate(pc.Auth, namespace, repository, d.Id(), opts)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(entitlement.SlugPerm)
+
+	return resourceEntitlementRead(d, m)
+}
+
+func resourceEntitlementDelete(d *schema.ResourceData, m interface{}) error {
+	pc := m.(*providerConfig)
+
+	namespace := d.Get("namespace").(string)
+	repository := d.Get("repository").(string)
+
+	_, err := pc.APIClient.EntitlementsApi.EntitlementsDelete(pc.Auth, namespace, repository, d.Id())
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+//nolint:funlen
+func resourceEntitlement() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceEntitlementCreate,
+		Read:   resourceEntitlementRead,
+		Update: resourceEntitlementUpdate,
+		Delete: resourceEntitlementDelete,
+
+		Schema: map[string]*schema.Schema{
+			"is_active": {
+				Type:        schema.TypeBool,
+				Description: "If enabled, the token will allow downloads based on configured restrictions (if any).",
+				Optional:    true,
+				Computed:    true,
+			},
+			"limit_date_range_from": {
+				Type:        schema.TypeString,
+				Description: "The starting date/time the token is allowed to be used from.",
+				Optional:    true,
+			},
+			"limit_date_range_to": {
+				Type:        schema.TypeString,
+				Description: "The ending date/time the token is allowed to be used until.",
+				Optional:    true,
+			},
+			"limit_num_clients": {
+				Type: schema.TypeInt,
+				Description: "The maximum number of unique clients allowed for the token. Please " +
+					"note that since clients are calculated asynchronously (after the download " +
+					"happens), the limit may not be imposed immediately but at a later point.",
+				Optional: true,
+				Computed: true,
+			},
+			"limit_num_downloads": {
+				Type: schema.TypeInt,
+				Description: "The maximum number of downloads allowed for the token. Please note " +
+					"that since downloads are calculated asynchronously (after the download " +
+					"happens), the limit may not be imposed immediately but at a later point.",
+				Optional: true,
+				Computed: true,
+			},
+			"limit_package_query": {
+				Type: schema.TypeString,
+				Description: "The package-based search query to apply to restrict downloads to. " +
+					"This uses the same syntax as the standard search used for repositories, and " +
+					"also supports boolean logic operators such as OR/AND/NOT and parentheses for " +
+					"grouping. This will still allow access to non-package files, such as metadata.",
+				Optional: true,
+			},
+			"limit_path_query": {
+				Type: schema.TypeString,
+				Description: "The path-based search query to apply to restrict downloads to. This " +
+					"supports boolean logic operators such as OR/AND/NOT and parentheses for " +
+					"grouping. The path evaluated does not include the domain name, the namespace, " +
+					"the entitlement code used, the package format, etc. and it always starts with " +
+					"a forward slash.",
+				Optional: true,
+			},
+			"name": {
+				Type:         schema.TypeString,
+				Description:  "A descriptive name for the entitlement.",
+				Required:     true,
+				ValidateFunc: validation.StringIsNotEmpty,
+			},
+			"namespace": {
+				Type:         schema.TypeString,
+				Description:  "Namespace to which this entitlement belongs.",
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.StringIsNotEmpty,
+			},
+			"repository": {
+				Type:         schema.TypeString,
+				Description:  "Repository to which this entitlement belongs.",
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.StringIsNotEmpty,
+			},
+			"token": {
+				Type:         schema.TypeString,
+				Description:  "The literal value of the token to be created.",
+				Optional:     true,
+				Computed:     true,
+				ValidateFunc: validation.StringIsNotEmpty,
+			},
+		},
+	}
+}
---
+++ cloudsmith/resource_entitlement_test.go
@@ -0,0 +1,135 @@
+//nolint:testpackage
+package cloudsmith
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+)
+
+// TestAccEntitlement_basic spins up a repository with all default options,
+// creates an entitlement with default options and verifies it exists and checks
+// the name is set correctly. Then it changes the name and some of the limit
+// variables, and verifies they've been set correctly before tearing down the
+// resources and verifying deletion.
+func TestAccEntitlement_basic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccEntitlementCheckDestroy("cloudsmith_entitlement.test"),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccEntitlementConfigBasic,
+				Check: resource.ComposeTestCheckFunc(
+					testAccEntitlementCheckExists("cloudsmith_entitlement.test"),
+					resource.TestCheckResourceAttr("cloudsmith_entitlement.test", "name", "Test Entitlement"),
+					resource.TestCheckResourceAttr("cloudsmith_entitlement.test", "limit_num_downloads", "0"),
+				),
+			},
+			{
+				Config: testAccEntitlementConfigBasicUpdate,
+				Check: resource.ComposeTestCheckFunc(
+					testAccEntitlementCheckExists("cloudsmith_entitlement.test"),
+					resource.TestCheckResourceAttr("cloudsmith_entitlement.test", "name", "Test Entitlement Update"),
+					resource.TestCheckResourceAttr("cloudsmith_entitlement.test", "limit_num_downloads", "100"),
+				),
+			},
+		},
+	})
+}
+
+//nolint:goerr113
+func testAccEntitlementCheckDestroy(resourceName string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		resourceState, ok := s.RootModule().Resources[resourceName]
+		if !ok {
+			return fmt.Errorf("resource not found: %s", resourceName)
+		}
+
+		if resourceState.Primary.ID == "" {
+			return fmt.Errorf("resource id not set")
+		}
+
+		pc := testAccProvider.Meta().(*providerConfig)
+
+		namespace := os.Getenv("CLOUDSMITH_NAMESPACE")
+		repository := resourceState.Primary.Attributes["repository"]
+		entitlement := resourceState.Primary.ID
+
+		_, resp, err := pc.APIClient.EntitlementsApi.EntitlementsRead(pc.Auth, namespace, repository, entitlement, nil)
+		if err != nil {
+			if err.Error() != errMessage404 {
+				return fmt.Errorf("unable to verify entitlement deletion: %w", err)
+			}
+		}
+		defer resp.Body.Close()
+
+		_, resp, err = pc.APIClient.ReposApi.ReposRead(pc.Auth, namespace, repository)
+		if err != nil {
+			if err.Error() != errMessage404 {
+				return fmt.Errorf("unable to verify repository deletion: %w", err)
+			}
+		}
+		defer resp.Body.Close()
+
+		return nil
+	}
+}
+
+//nolint:goerr113
+func testAccEntitlementCheckExists(resourceName string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		resourceState, ok := s.RootModule().Resources[resourceName]
+		if !ok {
+			return fmt.Errorf("resource not found: %s", resourceName)
+		}
+
+		if resourceState.Primary.ID == "" {
+			return fmt.Errorf("resource id not set")
+		}
+
+		pc := testAccProvider.Meta().(*providerConfig)
+
+		namespace := os.Getenv("CLOUDSMITH_NAMESPACE")
+		repository := resourceState.Primary.Attributes["repository"]
+		entitlement := resourceState.Primary.ID
+
+		_, resp, err := pc.APIClient.EntitlementsApi.EntitlementsRead(pc.Auth, namespace, repository, entitlement, nil)
+		if err != nil {
+			return fmt.Errorf("unable to verify entitlement existence: %w", err)
+		}
+		defer resp.Body.Close()
+
+		return nil
+	}
+}
+
+var testAccEntitlementConfigBasic = fmt.Sprintf(`
+resource "cloudsmith_repository" "test" {
+	name      = "terraform-acc-test-ent"
+	namespace = "%s"
+}
+
+resource "cloudsmith_entitlement" "test" {
+    name                = "Test Entitlement"
+    namespace           = "${cloudsmith_repository.test.namespace}"
+    repository          = "${cloudsmith_repository.test.slug_perm}"
+}
+`, os.Getenv("CLOUDSMITH_NAMESPACE"))
+
+var testAccEntitlementConfigBasicUpdate = fmt.Sprintf(`
+resource "cloudsmith_repository" "test" {
+	name      = "terraform-acc-test-ent"
+	namespace = "%s"
+}
+
+resource "cloudsmith_entitlement" "test" {
+	name                = "Test Entitlement Update"
+    limit_num_downloads = 100
+    namespace           = "${cloudsmith_repository.test.namespace}"
+    repository          = "${cloudsmith_repository.test.slug_perm}"
+}
+`, os.Getenv("CLOUDSMITH_NAMESPACE"))
