dd755226400e7a1737a4e5fbc931eeb9313bc9bf
Paddy Carey <pcarey@cloudsmith.io>
2022-12-05 18:48:34 +0000

    fix: test

--- cloudsmith/data_source_repository_test.go
+++ cloudsmith/data_source_repository_test.go
@@ -30,7 +30,7 @@ func TestAccRepository_data(t *testing.T) {
 					resource.TestCheckResourceAttr("data.cloudsmith_repository.test", "contextual_auth_realm", "true"),
 					resource.TestCheckResourceAttr("data.cloudsmith_repository.test", "docker_refresh_tokens_enabled", "false"),
 					resource.TestCheckResourceAttr("data.cloudsmith_repository.test", "resync_own", "true"),
-					resource.TestCheckResourceAttr("data.cloudsmith_repository.test", "resync_packages", "Write"),
+					resource.TestCheckResourceAttr("data.cloudsmith_repository.test", "resync_packages", "Admin"),
 					resource.TestCheckResourceAttr("data.cloudsmith_repository.test", "use_vulnerability_scanning", "true"),
 				),
 			},
