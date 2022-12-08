3db92ce4da2bbc5d6d35ed5d66160c6bdbef6133
Patrick Carey <pcarey@cloudsmith.io>
2022-12-08 13:10:31 +0000

    feat: add all available repository settings to resource (#25)

--- cloudsmith/resource_repository_test.go
+++ cloudsmith/resource_repository_test.go
@@ -4,6 +4,7 @@ package cloudsmith
 import (
 	"fmt"
 	"os"
+	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
@@ -14,6 +15,10 @@ import (
 // verifies it exists and checks the name is set correctly. Then it changes the
 // name, and verifies it's been set correctly before tearing down the resource
 // and verifying deletion.
+//
+// NOTE: It is not necessary to check properties that have been explicitly set
+// as Terraform performs a drift/plan check after every step anyway. Only
+// computed properties need explicitly checked.
 func TestAccRepository_basic(t *testing.T) {
 	t.Parallel()
 
@@ -26,14 +31,31 @@ func TestAccRepository_basic(t *testing.T) {
 				Config: testAccRepositoryConfigBasic,
 				Check: resource.ComposeTestCheckFunc(
 					testAccRepositoryCheckExists("cloudsmith_repository.test"),
-					resource.TestCheckResourceAttr("cloudsmith_repository.test", "name", "terraform-acc-test"),
+					// check a sample of computed properties have been set correctly
+					resource.TestCheckResourceAttr("cloudsmith_repository.test", "contextual_auth_realm", "true"),
+					resource.TestCheckResourceAttr("cloudsmith_repository.test", "copy_own", "true"),
+					resource.TestCheckResourceAttr("cloudsmith_repository.test", "copy_packages", "Read"),
+					resource.TestCheckResourceAttr("cloudsmith_repository.test", "docker_refresh_tokens_enabled", "false"),
+					resource.TestCheckResourceAttr("cloudsmith_repository.test", "is_private", "true"),
+					resource.TestCheckResourceAttr("cloudsmith_repository.test", "is_public", "false"),
+					resource.TestCheckResourceAttr("cloudsmith_repository.test", "replace_packages_by_default", "false"),
+					resource.TestCheckResourceAttr("cloudsmith_repository.test", "use_vulnerability_scanning", "true"),
 				),
 			},
 			{
 				Config: testAccRepositoryConfigBasicUpdateName,
 				Check: resource.ComposeTestCheckFunc(
 					testAccRepositoryCheckExists("cloudsmith_repository.test"),
-					resource.TestCheckResourceAttr("cloudsmith_repository.test", "name", "terraform-acc-test-update"),
+				),
+			},
+			{
+				Config:      testAccRepositoryConfigBasicInvalidProp,
+				ExpectError: regexp.MustCompile("expected copy_packages to be one of"),
+			},
+			{
+				Config: testAccRepositoryConfigBasicUpdateProps,
+				Check: resource.ComposeTestCheckFunc(
+					testAccRepositoryCheckExists("cloudsmith_repository.test"),
 				),
 			},
 		},
@@ -105,5 +127,27 @@ var testAccRepositoryConfigBasicUpdateName = fmt.Sprintf(`
 resource "cloudsmith_repository" "test" {
 	name      = "terraform-acc-test-update"
 	namespace = "%s"
+}
+`, os.Getenv("CLOUDSMITH_NAMESPACE"))
+
+var testAccRepositoryConfigBasicInvalidProp = fmt.Sprintf(`
+resource "cloudsmith_repository" "test" {
+	name      = "terraform-acc-test-update"
+	namespace = "%s"
+
+	copy_packages = "Sudo"
+}
+`, os.Getenv("CLOUDSMITH_NAMESPACE"))
+
+var testAccRepositoryConfigBasicUpdateProps = fmt.Sprintf(`
+resource "cloudsmith_repository" "test" {
+	name      = "terraform-acc-test-update"
+	namespace = "%s"
+
+	contextual_auth_realm         = false
+	copy_packages                 = "Write"
+	docker_refresh_tokens_enabled = true
+	replace_packages_by_default   = true
+	use_vulnerability_scanning    = false
 }
 `, os.Getenv("CLOUDSMITH_NAMESPACE"))
