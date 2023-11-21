55cd902f094d1e346be6a9335dcec8a08da66d83
Bartosz Blizniak <bblizniak@cloudsmith.io>
2023-11-21 14:46:43 +0000

    add support for reading repo privilages

---
+++ cloudsmith/data_source_repository_privileges.go
@@ -0,0 +1,106 @@
+package cloudsmith
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+// dataSourceRepositoryPrivileges returns the data source schema and read function.
+func dataSourceRepositoryPrivileges() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceRepositoryPrivilegesRead,
+
+		Schema: map[string]*schema.Schema{
+			"organization": {
+				Type:        schema.TypeString,
+				Description: "Organization to which this repository belongs.",
+				Required:    true,
+			},
+			"repository": {
+				Type:        schema.TypeString,
+				Description: "Repository to fetch privileges information.",
+				Required:    true,
+			},
+			"service": {
+				Type:     schema.TypeSet,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"privilege": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"slug": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"team": {
+				Type:     schema.TypeSet,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"privilege": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"slug": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"user": {
+				Type:     schema.TypeSet,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"privilege": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"slug": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+// dataSourceRepositoryPrivilegesRead retrieves privileges information for the specified repository.
+func dataSourceRepositoryPrivilegesRead(d *schema.ResourceData, m interface{}) error {
+	pc := m.(*providerConfig)
+
+	organization := d.Get("organization").(string)
+	repository := d.Get("repository").(string)
+
+	req := pc.APIClient.ReposApi.ReposPrivilegesList(pc.Auth, organization, repository)
+
+	req = req.Page(1)
+	req = req.PageSize(1000)
+
+	privileges, resp, err := pc.APIClient.ReposApi.ReposPrivilegesListExecute(req)
+	if err != nil {
+		if is404(resp) {
+			d.SetId("")
+			return nil
+		}
+
+		return err
+	}
+
+	d.Set("service", flattenRepositoryPrivilegeServices(privileges.GetPrivileges()))
+	d.Set("team", flattenRepositoryPrivilegeTeams(privileges.GetPrivileges()))
+	d.Set("user", flattenRepositoryPrivilegeUsers(privileges.GetPrivileges()))
+
+	d.SetId(fmt.Sprintf("%s/%s", organization, repository))
+
+	return nil
+}
---
+++ cloudsmith/data_source_repository_privileges_test.go
@@ -0,0 +1,58 @@
+package cloudsmith
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+// TestAccDataSourceRepositoryPrivileges_basic tests the basic functionality of the data source.
+func TestAccDataSourceRepositoryPrivileges_basic(t *testing.T) {
+	t.Parallel()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccRepositoryCheckDestroy("cloudsmith_repository_privileges.test_data"),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceRepositoryPrivilegesConfigBasic,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("data.cloudsmith_repository_privileges.test_data", "service.#"),
+					resource.TestCheckResourceAttrSet("data.cloudsmith_repository_privileges.test_data", "team.#"),
+					resource.TestCheckResourceAttrSet("data.cloudsmith_repository_privileges.test_data", "user.#"),
+				),
+			},
+		},
+	})
+}
+
+var testAccDataSourceRepositoryPrivilegesConfigBasic = fmt.Sprintf(`
+resource "cloudsmith_repository" "test" {
+	name      = "terraform-acc-test-privs"
+	namespace = "%s"
+}
+
+resource "cloudsmith_service" "test" {
+	name         = "TF Test Service Privs"
+	organization = cloudsmith_repository.test.namespace
+	role         = "Member"
+}
+
+resource "cloudsmith_repository_privileges" "test" {
+    organization = cloudsmith_repository.test.namespace
+    repository   = cloudsmith_repository.test.slug
+
+	service {
+		privilege = "Read"
+		slug      = cloudsmith_service.test.slug
+	}
+}
+
+data "cloudsmith_repository_privileges" "test_data" {
+  organization = cloudsmith_repository.test.namespace
+  repository   = cloudsmith_repository.test.slug
+}
+`, os.Getenv("CLOUDSMITH_NAMESPACE"))
