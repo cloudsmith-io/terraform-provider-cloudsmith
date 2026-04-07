049a1d0e3d4afe43640779d7a4677ef8fb30eb37
BB <55028730+BartoszBlizniak@users.noreply.github.com>
2026-04-07 15:07:15 +0100

    Original work by @RoseSecurity in #192 (#194)
    * feat: add debug mode support for provider development
    
    Add a debug flag to enable running the provider with debugger support
    (e.g., delve). This allows developers to attach debuggers during
    provider development and troubleshooting.
    
    * chore: add ProviderAddr
    
    ---------
    
    Co-authored-by: RoseSecurity <michael@rosesecurity.dev>

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/cloudsmith-io/terraform-provider-cloudsmith/cloudsmith"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
 
 func main() {
+	var debugMode bool
+
+	flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: cloudsmith.Provider,
+		ProviderAddr: "registry.terraform.io/cloudsmith-io/cloudsmith",
+		Debug:        debugMode,
 	})
 }
