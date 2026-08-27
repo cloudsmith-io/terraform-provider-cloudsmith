c259cb2b80ead4461f52553db695358182f0d2b0
David Lynch <dlynch@cloudsmith.io>
2026-08-27 15:23:25 +0100

    fix(ENG-12892): prevent repo retention rule state clobbering
    When multiple retention rules are applied to a repository, the values of the rules are clobbered between applies.
    In the below example, when multiple applies are run, the retention rule will be set to the values of the other rule using terraform patch.
    
    ```terraform
    resource "cloudsmith_repository" "test-retention" {
    	name      = "bla"
    	namespace = "blabla"
    }
    
    resource "cloudsmith_repository_retention_rule" "test_a" {
    	namespace = cloudsmith_repository.test-retention.namespace
    	repository = cloudsmith_repository.test-retention.name
    	retention_enabled = false
    	retention_count_limit = 100
    	retention_days_limit = 20
    	retention_group_by_name = false
    	retention_group_by_format = false
    	retention_group_by_package_type = false
    	retention_size_limit = 10
    	retention_package_query_string = "name:test"
    }
    
    resource "cloudsmith_repository_retention_rule" "test_b" {
    	namespace = cloudsmith_repository.test-retention.namespace
    	repository = cloudsmith_repository.test-retention.name
    	retention_enabled = false
    	retention_count_limit = 50
    	retention_days_limit = 12
    	retention_group_by_name = false
    	retention_group_by_format = false
    	retention_group_by_package_type = false
    	retention_size_limit = 49
    	retention_package_query_string = "name:test"
    }
    `
    
    ```

--- cloudsmith/resource_repository_retention_rule_test.go
+++ cloudsmith/resource_repository_retention_rule_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"testing"
+	"regexp"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
@@ -65,6 +66,9 @@ func TestAccRepositoryRetentionRule_basic(t *testing.T) {
 					testCheckResourceAttrWithMessage("cloudsmith_repository_retention_rule.test", "retention_days_limit", "0"),
 					testCheckResourceAttrWithMessage("cloudsmith_repository_retention_rule.test", "retention_size_limit", "0"),
 				),
+			}, {
+				Config:      testAccRepositoryRetentionRuleDuplicate(repositoryName),
+				ExpectError: regexp.MustCompile(""),
 			},
 		},
 		CheckDestroy: testAccRepositoryCheckDestroy("cloudsmith_repository.test-retention"),
@@ -119,6 +123,40 @@ resource "cloudsmith_repository_retention_rule" "test" {
 	retention_group_by_format = false
 	retention_group_by_package_type = false
 	retention_size_limit = 0
+	retention_package_query_string = "name:test"
+}
+`, repositoryName, os.Getenv("CLOUDSMITH_NAMESPACE"))
+}
+
+func testAccRepositoryRetentionRuleDuplicate(repositoryName string) string {
+	return fmt.Sprintf(`resource "cloudsmith_repository" "test-retention" {
+	name      = "%s"
+	namespace = "%s"
+}
+
+resource "cloudsmith_repository_retention_rule" "test_a" {
+	namespace = cloudsmith_repository.test-retention.namespace
+	repository = cloudsmith_repository.test-retention.name
+	retention_enabled = false
+	retention_count_limit = 100
+	retention_days_limit = 20
+	retention_group_by_name = false
+	retention_group_by_format = false
+	retention_group_by_package_type = false
+	retention_size_limit = 10
+	retention_package_query_string = "name:test"
+}
+
+resource "cloudsmith_repository_retention_rule" "test_b" {
+	namespace = cloudsmith_repository.test-retention.namespace
+	repository = cloudsmith_repository.test-retention.name
+	retention_enabled = false
+	retention_count_limit = 50
+	retention_days_limit = 12
+	retention_group_by_name = false
+	retention_group_by_format = false
+	retention_group_by_package_type = false
+	retention_size_limit = 49
 	retention_package_query_string = "name:test"
 }
 `, repositoryName, os.Getenv("CLOUDSMITH_NAMESPACE"))
